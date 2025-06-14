e312fbf527001daf7abbf1e9138df21a573d7e5d
ChethanRaj13 <chethanrajjee@gmail.com>
2025-06-14 06:42:14 +0530

    completed the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,63 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
+
 void main() {
-  return runApp(MyApp as Widget);
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.red.shade200,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Center(child: Text('Dicey', style: TextStyle(color: Colors.white),),),
+          backgroundColor: Colors.red.shade300,
         ),
         body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int left = 1;
+  int right = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+                onPressed: (){
+                  setState(() {
+                    left = Random().nextInt(6)+1;
+                  });
+                },
+                  child: Image.asset('images/dice$left.png'),
+              ),
+          ),
+          Expanded(
+              child: TextButton(
+                onPressed: (){
+                  setState(() {
+                    right = Random().nextInt(6)+1;
+                  });
+                },
+                  child: Image.asset('images/dice$right.png'),
+              ),
+          ),
+        ],
       ),
     );
   }
 }
 
 
-class DicePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+
